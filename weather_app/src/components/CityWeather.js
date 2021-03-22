5e04ec7e044a043d2b921a802845899fc57c0bbe
Dima <dimhukys@gmail.com>
2021-03-22 17:49:42 +0200

    Wrong eslint installation. Correct version

--- weather_app/src/components/CityWeather.js
+++ weather_app/src/components/CityWeather.js
@@ -2,11 +2,13 @@ import "./Styles/CityWeather.css"
 
 const CityWeather = (props) => {
     const image_url = `http://openweathermap.org/img/wn/${props.weather[0].icon}@2x.png`;
+    const {temp, name} = props;
     return (
+
         <div>
-            <h1>{props.name}</h1>
-            <p>{props.weather[0].description}</p>
-            <p>Temp: {Math.floor(props.main.temp - 273.15)}</p>
+            <h1>{name}</h1>
+            {/*<p>{props.weather[0].description}</p>*/}
+            <p>Temp: {Math.floor(temp - 273.15)}</p>
             <img src={image_url}/>
         </div>
     )
